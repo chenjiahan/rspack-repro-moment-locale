9598f93e12fab024e5f70c0ac7cfc7afa533733f
chenjiahan <chenjiahan.jait@bytedance.com>
2024-01-23 10:42:02 +0800

    chore: init

--- config.mjs
+++ config.mjs
@@ -1,6 +1,7 @@
 import path from "path";
 import { fileURLToPath } from "url";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const isRunningWebpack = !!process.env.WEBPACK;
@@ -18,7 +19,7 @@ const config = {
   entry: {
     main: "./src/index",
   },
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [new HtmlWebpackPlugin(), new BundleAnalyzerPlugin()],
   output: {
     clean: true,
     path: isRunningWebpack
